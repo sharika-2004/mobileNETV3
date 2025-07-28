6b48a7f6ee324d9bc50b5ecf8abc68f06a8eff49
Sharika P K <sharikavipanchika@gmail.com>
2025-07-28 11:44:24 +0530

    Create accuracy-loss_curve.py

---
+++ Testing/accuracy-loss_curve.py
@@ -0,0 +1,13 @@
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Accuracy Curves')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Loss Curves')
+plt.legend()
+plt.show()
